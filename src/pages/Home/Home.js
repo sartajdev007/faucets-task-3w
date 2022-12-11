cb7b39a2e19a1269cb83c9efb54ae896dc5b4f57
sartajdev007 <sartaj5akik@gmail.com>
2022-12-12 00:32:57 +0600

    home form add

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-
-        </div>
+        <Container className='pt-5'>
+            <div>
+                <h1 className='fw-bold color1'>Request Testnet LINK</h1>
+                <p className='w-50 pt-2'>
+                    Get testnet LINK for an account on one of the supported blockchain testnets so you can create and test your own oracle and Chainlinked smart contract
+                </p>
+            </div>
+            <div className='form-container mt-5'>
+                <div className='warning'>
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M12.866 3l9.526 16.5a1 1 0 0 1-.866 1.5H2.474a1 1 0 0 1-.866-1.5L11.134 3a1 1 0 0 1 1.732 0zM11 16v2h2v-2h-2zm0-7v5h2V9h-2z"></path></g></svg>
+                    <p>Your wallet is connected to <span className='fw-bold'>Ethereum Kovan</span>, so you are requesting <span className='fw-bold'>Ethereum Kovan</span> Link/ETH.</p>
+                </div>
+                <div className='w-50 pt-2'>
+                    <div class="mb-3">
+                        <label for="" class="form-label" className='color1 fs-6 fw-bold'>Wallet Address</label>
+                        <input type="text" class="form-control" id="" placeholder="Wallet Address..." />
+                    </div>
+                    <div class="row g-3 mb-3">
+                        <div class="col">
+                            <label htmlFor="" className='color1 fs-6 fw-bold'>Request Type</label>
+                            <input type="text" class="form-control" placeholder="First name" aria-label="First name" />
+                        </div>
+                        <div class="col">
+                            <label htmlFor="">{' '}</label>
+                            <input type="text" class="form-control" placeholder="Last name" aria-label="Last name" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Container>
     );
 };
 
--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -15,47 +15,56 @@ const Header = () => {
 
 
     return (
-        <Navbar bg="light" expand="lg">
-            <Container className='d-flex justify-content-between'>
-                <div>
-                    <Navbar.Brand href="/" className='color1 fw-bold' >Faucets</Navbar.Brand>
-                </div>
-                <div>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Dropdown id="basic-nav-dropdown">
-                                <Dropdown.Toggle className='border border-1 border-secondary rounded-1 px-3' variant="" id="dropdown-basic">
-                                    Ethereum Kovan
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu className='rounded-0'>
-                                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            <Button className='rounded-1 ms-2 color1 border1 modalbtn px-3' variant='outline' onClick={() => setShowModal(true)}>
-                                <IoWallet></IoWallet> Connect Wallet
-                            </Button>
-                            <WalletModal
-                                show={showModal}
-                                onHide={() => setShowModal(false)}
-                            />
-                            <Dropdown>
-                                <Dropdown.Toggle className='border-0 rounded-5 ms-2 bg-transparent' id="dropdown-basic">
-                                    <FaRegUserCircle className='fs-4 text-secondary'></FaRegUserCircle>
-                                </Dropdown.Toggle>
+        <div>
+            <Navbar bg="light" expand="lg" className=' py-3'>
+                <Container className='d-flex justify-content-between'>
+                    <div>
+                        <Navbar.Brand href="/" className='color1 fw-bold' >Faucets</Navbar.Brand>
+                    </div>
+                    <div>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Dropdown id="basic-nav-dropdown">
+                                    <Dropdown.Toggle className='border border-1 border-secondary rounded-1 px-3' variant="" id="dropdown-basic">
+                                        Ethereum Kovan
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu className='rounded-0'>
+                                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                                <Button className='rounded-1 ms-2 color1 border1 modalbtn px-3' variant='outline' onClick={() => setShowModal(true)}>
+                                    <IoWallet></IoWallet> Connect Wallet
+                                </Button>
+                                <WalletModal
+                                    show={showModal}
+                                    onHide={() => setShowModal(false)}
+                                />
+                                <Dropdown>
+                                    <Dropdown.Toggle className='border-0 rounded-5 ms-2 bg-transparent' id="dropdown-basic">
+                                        <FaRegUserCircle className='fs-4 text-secondary'></FaRegUserCircle>
+                                    </Dropdown.Toggle>
 
-                                <Dropdown.Menu className='p-2 rounded-0'>
-                                    <Dropdown.Item><Link to='/login' className='text-decoration-none text-body'>Log In</Link></Dropdown.Item>
-                                    <Dropdown.Item><Link to='/register' className='text-decoration-none text-body'>Sign Up</Link></Dropdown.Item>
-                                    <Dropdown.Item><Link to='/faq' className='text-decoration-none text-body'>FAQ</Link></Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Nav>
-                    </Navbar.Collapse>
+                                    <Dropdown.Menu className='p-2 rounded-0'>
+                                        <Dropdown.Item><Link to='/login' className='text-decoration-none text-body'>Log In</Link></Dropdown.Item>
+                                        <Dropdown.Item><Link to='/register' className='text-decoration-none text-body'>Sign Up</Link></Dropdown.Item>
+                                        <Dropdown.Item><Link to='/faq' className='text-decoration-none text-body'>FAQ</Link></Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </div>
+                </Container>
+            </Navbar>
+            <div className='notifysection py-3'>
+                <div className='container'>
+                    <div className='text-white'>
+                        <p className='text-center'>Notice Here</p>
+                    </div>
                 </div>
-            </Container>
-        </Navbar>
+            </div>
+        </div>
     );
 };
 
